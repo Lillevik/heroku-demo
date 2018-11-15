de76ca47fde9191c356429ab78ea8dfd98205174
Marius Lillevik <mariusfalch@hotmail.com>
2018-11-15 18:36:36 +0100

    ove var her

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        Learn React
+                        Learn React ove var her
                     </a>
                 </header>
             </div>
